a95d568d82e1d9470f744d545d0a6d3a885745da
Prinz Magtulis <87161563+pmagtulis@users.noreply.github.com>
2022-02-05 00:28:56 -0500

    Add files via upload

--- autoscraper.py
+++ autoscraper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 import pandas as pd
@@ -13,6 +13,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 from selenium import webdriver
+
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
@@ -48,41 +49,24 @@ driver.get("https://www.foi.gov.ph/requests")
 
 dataset = []
 while True:
-    try:
-        tabs = driver.find_elements(By.CSS_SELECTOR, ".col-xxs-12.col-xs-12.col-sm-8")
-        for tab in tabs:
-            try:
-                all_div = tab.find_elements(By.CSS_SELECTOR, '.component-panel')
-            except:
-                break
-            for div in all_div[1:]:
-                data={}
-                data ['agency'] = div.find_element(By.TAG_NAME, 'span').text
-                data ['date'] = div.find_element(By.TAG_NAME, 'p').get_attribute('title')
-                data ['title'] = div.find_element(By.TAG_NAME, 'h4').text
-                data ['status'] = div.find_element(By.TAG_NAME, 'label').text
-                data ['purpose'] = div.find_elements(By.TAG_NAME, 'span')[2].text
-                data ['period_covered'] = div.find_elements(By.TAG_NAME, 'span')[3].text
-                data ['link'] = div.find_element(By.TAG_NAME, 'a').get_attribute('href')
-                dataset.append(data)
-    except:
+    all_div = driver.find_elements(By.CSS_SELECTOR, ".mb10")
+    if len(dataset) >= 3000:
         break
+    for div in all_div:
+        data={}
+        data ['agency'] = div.find_element(By.TAG_NAME, 'span').text
+        data ['date'] = div.find_element(By.TAG_NAME, 'p').get_attribute('title')
+        data ['title'] = div.find_element(By.TAG_NAME, 'h4').text
+        data ['status'] = div.find_element(By.TAG_NAME, 'label').text
+        data ['purpose'] = div.find_elements(By.TAG_NAME, 'span')[2].text
+        data ['period_covered'] = div.find_elements(By.TAG_NAME, 'span')[3].text
+        data ['link'] = div.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        dataset.append(data)
+    
+    driver.find_element(By.XPATH, "/html/body/section/div/div/div/div[2]/div/div/div/a").click()
 
-    try:
-        driver.find_element(By.XPATH, "/html/body/section/div/div/div/div[2]/div/div/div/a").click()
-    except:
-        print("Nothing more.")
-        break
 
-
-# In[4]:
-
-
-df = pd.DataFrame(dataset)
-df
-
-
-# In[5]:
+# In[8]:
 
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
