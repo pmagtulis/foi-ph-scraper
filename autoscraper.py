7f6a08a52f83d31587929a28f20eaf0b7fcb5193
Prinz Magtulis <87161563+pmagtulis@users.noreply.github.com>
2022-06-14 15:39:06 -0400

    Add files via upload

--- autoscraper.py
+++ autoscraper.py
@@ -15,7 +15,7 @@ warnings.filterwarnings("ignore")
 from selenium import webdriver
 
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
