b854d53d73d18d299f027215b91531e1aa615926
Prinz Magtulis <87161563+pmagtulis@users.noreply.github.com>
2022-07-10 22:52:08 -0400

    Update autoscraper.py

--- autoscraper.py
+++ autoscraper.py
@@ -21,7 +21,7 @@ warnings.filterwarnings("ignore")
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.service import Service as ChromiumService
+#from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.common.by import By
