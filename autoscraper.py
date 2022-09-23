456cb991412d3e0aeab4f646cde13d0f53d7b347
Prinz Magtulis <87161563+pmagtulis@users.noreply.github.com>
2022-09-22 22:01:58 -0400

    Add files via upload

--- autoscraper.py
+++ autoscraper.py
@@ -16,6 +16,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
